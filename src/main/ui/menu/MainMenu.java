5f897f2aec8fabd3d91d5dcbfceb79784345c15d
Patrick Perdomo <perdomo.patrick.s@gmail.com>
2024-10-24 00:48:46 -0700

    Added UI for data persistence

--- src/main/ui/menu/MainMenu.java
+++ src/main/ui/menu/MainMenu.java
@@ -1,10 +1,21 @@
 package ui.menu;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import model.Gallery;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
 // Main Menu
 public class MainMenu extends Menu {
 
     private final ViewMenu viewMenu;
     private final NewCanvasMenu newCanvasMenu;
+
+    private static final String JSON_STORE = "./data/gallery.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
 
     // EFFECTS: creates instances of ViewMenu and NewCanvasMenu,
     // and sets quit to false
@@ -12,6 +23,9 @@ public class MainMenu extends Menu {
         viewMenu = new ViewMenu();
         newCanvasMenu = new NewCanvasMenu();
         quit = false;
+
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
     }
 
     // EFFECTS: Main menu, gets the user's valid input then sends input to be
@@ -21,7 +35,7 @@ public class MainMenu extends Menu {
     public void menu() {
         String choice;
         do {
-            choice = getInputStr("new", "n", "view", "v", "quit", "q");
+            choice = getInputStr("new", "n", "view", "v", "quit", "q", "load", "l", "save", "s");
             evaluateStrInput(choice);
         } while (!quit);
     }
@@ -31,7 +45,9 @@ public class MainMenu extends Menu {
     public void prompt() {
         System.out.println("=== Main Menu ===");
         System.out.println("- New canvas (new, n)");
-        System.out.println("- View canvas (view, v)");
+        System.out.println("- View gallery (view, v)");
+        System.out.println("- Load saved gallery  (load, l)");
+        System.out.println("- Save gallery (save, s)");
         System.out.println("- Quit (quit, q)");
         System.out.print("Choose an option: ");
     }
@@ -41,6 +57,8 @@ public class MainMenu extends Menu {
     // action
     // If the command is "new" or "n", it opens the new canvas menu
     // If the command is "view" or "v", it opens the view canvas menu
+    // If the command is "load" or "l", it goes to load method
+    // If the command is "save" or "s", it goes to save method
     // If the command is "quit" or "q", it exits the game and sets the quit flag to
     // true
     // ignores case sensitivity for the command
@@ -53,6 +71,31 @@ public class MainMenu extends Menu {
         } else if (command.equalsIgnoreCase("quit") || command.equalsIgnoreCase("q")) {
             System.out.println("Exiting the game...");
             quit = true;
+        } else if (command.equalsIgnoreCase("load") || command.equalsIgnoreCase("l")) {
+        	load();
+        } else {
+        	save();
+        }
+    }
+
+    private void save() {
+		try {
+            jsonWriter.open();
+            jsonWriter.write(gallery);
+            jsonWriter.close();
+            System.out.println("Saved to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
+        }
+	}
+	
+	private void load() {
+        try {
+            Gallery deserializedGallery = jsonReader.read();
+            Gallery.getInstance().updateFrom(deserializedGallery);
+            System.out.println("Loaded from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
         }
     }
 }
