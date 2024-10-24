26da24b12548e303fc6e122a234becf9ae207051
Patrick Perdomo <perdomo.patrick.s@gmail.com>
2024-10-24 00:45:06 -0700

    Added implementation for JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -17,36 +17,66 @@ import model.Gallery;
 
 // Represents a reader that reads gallery from JSON data stored in file
 public class JsonReader {
+    private String source;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
+        this.source = source;
     }
 
     // EFFECTS: reads gallery from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Gallery read() throws IOException {
-        return null; // stub
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseGallery(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null; // stub
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses gallery from JSON object and returns it
     private Gallery parseGallery(JSONObject jsonObject) {
-        return null;// stub
+        Gallery g = new Gallery();
+        addCanvases(g, jsonObject);
+        return g;
     }
 
     // MODIFIES: g
     // EFFECTS: parses Canvases from JSON object and adds them to gallery
     private void addCanvases(Gallery g, JSONObject jsonObject) {
-        // stub
+        JSONArray jsonArray = jsonObject.getJSONArray("canvases");
+        for (Object json : jsonArray) {
+            JSONObject nextCanvas = (JSONObject) json;
+            addCanvas(g, nextCanvas);
+        }
     }
 
     // MODIFIES: g
     // EFFECTS: parses Canvas from JSON object and adds it to gallery
     private void addCanvas(Gallery g, JSONObject jsonObject) {
-        // stub
+        int stylusXCoord = jsonObject.getInt("stylusXCoord");
+        int stylusYCoord = jsonObject.getInt("stylusYCoord");
+        
+        JSONArray jsonArray = jsonObject.getJSONArray("canvas");
+        
+        boolean[][] board = new boolean[jsonArray.length()][jsonArray.getJSONArray(0).length()];    	
+		
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONArray innerArray = jsonArray.getJSONArray(i); 
+            for (int j = 0; j < innerArray.length(); j++) {
+            	board[i][j] = innerArray.getBoolean(j);  
+            }
+        }    	
+        Canvas canvas = new Canvas(board, stylusXCoord, stylusYCoord);
+        g.addCanvas(canvas);
     }
 }
