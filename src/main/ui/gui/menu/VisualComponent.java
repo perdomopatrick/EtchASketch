45ee4270a0e0d92cf9138bbc37d87b97f1bdbb6b
Patrick Perdomo <perdomo.patrick.s@gmail.com>
2024-11-21 22:32:21 -0800

    Fixed Checkstyle

--- src/main/ui/gui/menu/VisualComponent.java
+++ src/main/ui/gui/menu/VisualComponent.java
@@ -11,11 +11,16 @@ public class VisualComponent extends Menu {
 
     private String countryName;
     private JPanel panel;
+    private JLabel label;
     private static final String COUNTRY_PATH = "./data/countriesImages";
 
     // EFFECTS: Initializes VisualComponent a with the given mainMenu
     public VisualComponent(MainMenu mainMenu) {
         super(mainMenu);
+        
+        panel = new JPanel(new FlowLayout());
+        JLabel label = new JLabel();
+        panel.add(label);
     }
 
     // MODIFIES: this
@@ -23,33 +28,19 @@ public class VisualComponent extends Menu {
     // sets up the image, text field and buttons, 
     @Override
     public JPanel menu() {
-        panel = new JPanel(new FlowLayout());
-        JLabel label = new JLabel();
-        panel.add(label);
-
         JTextField textField = new JTextField(20);
         JButton enterButton = new JButton("Enter");
         JButton resetButton = new JButton("New Country");
         JButton quitButton = new JButton("Quit");
+
         quitButton.addActionListener(e -> mainMenu.showCard("MainMenu"));
-        getRandomCountryName(label);
+        getRandomCountryName(textField);
 
-        enterButton.addActionListener(e -> {
-            String userInput = textField.getText().toLowerCase().replaceAll("\\s+", "");
-
-            if (userInput.equalsIgnoreCase(countryName.replaceAll("\\s+", ""))) {
-                JOptionPane.showMessageDialog(panel, "Correct!");
-                textField.setText("");
-                getRandomCountryName(label);
-            } else {
-                JOptionPane.showMessageDialog(panel, "Incorrect! Try again!");
-            }
-        });
+        enterButtonListener(textField, enterButton);
 
         resetButton.addActionListener(e -> {
             JOptionPane.showMessageDialog(panel, "Country: " + countryName);
-            textField.setText("");
-            getRandomCountryName(label);
+            getRandomCountryName(textField);
         });
 
         panel.add(textField);
@@ -59,11 +50,28 @@ public class VisualComponent extends Menu {
         return panel;
     }
 
+    // EFFECTS: add action listener to give enterButton, when clicked checks 
+    // if text in give text field is equal to countryName, then displays dialog based on outcome
+    private void enterButtonListener(JTextField textField, JButton enterButton) {
+        enterButton.addActionListener(e -> {
+            String userInput = textField.getText().toLowerCase().replaceAll("\\s+", "");
+
+            if (userInput.equalsIgnoreCase(countryName)) {
+                JOptionPane.showMessageDialog(panel, "Correct!");
+                getRandomCountryName(textField);
+            } else {
+                JOptionPane.showMessageDialog(panel, "Incorrect! Try again!");
+            }
+        });
+    }
+
     // REQUIRES: label != null
     // MODIFIES: this
     // EFFECTS: Sets the countryName to a random country image file from the COUNTRY_PATH directory and 
     // updates the provided gievm label with the selected country image
-    private void getRandomCountryName(JLabel label) {
+    private void getRandomCountryName(JTextField textField) {
+        textField.setText("");
+
         File folder = new File(COUNTRY_PATH);
         File[] files = folder.listFiles();
         Random random = new Random();
